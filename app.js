7691398dc18056a299329e93c98f321e53ed4d0a
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-25 10:49:33 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const { ObjectId } = require('mongodb')
+const { getDb, connectToDb } = require('./db')
+
+// init app & middleware
+const app = express()
+app.use(express.json())
+
+// db connection
+let db
+
+connectToDb((err) => {
+  if(!err){
+    app.listen('3000', () => {
+      console.log('app listening on port 3000')
+    })
+    db = getDb()
+  }
+})
+
+// routes
+app.get('/books', (req, res) => {
+  let books = []
+
+  db.collection('books')
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
+})
+
+app.get('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {
+
+    db.collection('books')
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(doc => {
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
+      
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+
+})
+
+app.post('/books',(req, res)=>{ 
+  const book = req.body
+
+  db.collection('books')
+  .insertOne(book)
+  .then(result => {
+    res.status(201).json(result)
+
+  })
+  .catch(err => {
+    res.status(500).json({ err:'Could not create a new document'})
+  })
+})
+

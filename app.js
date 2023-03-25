c875dd532da9721d802d7363be20e495432f2796
ㅇ ㅠ ㄴ ㅅ ㅐ ㅁ <48478079+PhoebeYoon@users.noreply.github.com>
2023-03-25 09:50:32 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const { ObjectId } = require('mongodb')
+const { getDb, connectToDb } = require('./db')
+
+// init app & middleware
+const app = express()
+
+// db connection
+let db
+
+connectToDb((err) => {
+  if(!err){
+    app.listen('3000', () => {
+      console.log('app listening on port 3000')
+    })
+    db = getDb()
+  }
+})
+
+// routes
+app.get('/books', (req, res) => {
+  let books = []
+
+  db.collection('books')
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
+})
+
+app.get('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {
+
+    db.collection('books')
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(doc => {
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
+      
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+
+})
